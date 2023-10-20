83407951fc835a8b5f42023e45a875c3d87a43bb
jaywcjlove <398188662@qq.com>
2023-10-20 16:02:26 +0800

    website: fix vite config.

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -5,6 +5,7 @@ import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/vue-markdown-preview',
   plugins: [
     vue(),
     raw({
